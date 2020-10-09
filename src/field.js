9f02cd65e7268c82827959a75dd039bb1853d61c
Ahron Kwon <kwonahron@gmail.com>
2020-10-09 16:14:10 +0900

    Refactoring for maintenance of field features

---
+++ src/field.js
@@ -0,0 +1,67 @@
+"use strict";
+
+const carrotSound = new Audio("./sound/carrot_pull.mp3");
+const CARROT_SIZE = 80;
+
+export default class Field {
+  constructor(carrotCount, bugCount, crazyBug) {
+    this.carrotCount = carrotCount;
+    this.bugCount = bugCount;
+    this.crazyBug = crazyBug;
+    this.itemArea = document.querySelector(".item-area");
+    this.itemBoundary = this.itemArea.getBoundingClientRect();
+    // this.onClick = this.onClick.bind(this);
+    this.itemArea.addEventListener("click", (event) => this.onClick(event));
+  }
+
+  init() {
+    this.itemArea.innerHTML = "";
+    this._deployment("carrot", this.carrotCount, "./img/carrot.png");
+    this._deployment("bug", this.bugCount, "./img/bug.png");
+    this._deployment("bug--crazy", this.crazyBug, "./img/crazybug.png");
+  }
+
+  setClickListener(onItemClick) {
+    this.onItemClick = onItemClick;
+  }
+
+  _deployment(className, number, imgPath) {
+    //private 한 함수를 만들기위해 _ 를 추가 외부에서 부르지 말것!
+    const x = 0;
+    const y = 0;
+    const widthX = this.itemBoundary.width - CARROT_SIZE;
+    const heightY = this.itemBoundary.height - CARROT_SIZE;
+
+    for (let i = 0; i < number; i++) {
+      const item = document.createElement("img");
+      item.setAttribute("class", className);
+      item.setAttribute("src", imgPath);
+      item.style.position = "absolute";
+      const ranX = randomNumber(x, widthX);
+      const ranY = randomNumber(y, heightY);
+      item.style.left = `${ranX}px`;
+      item.style.top = `${ranY}px`;
+      this.itemArea.appendChild(item);
+    }
+  }
+
+  onClick = (event) => {
+    const target = event.target;
+    if (target.matches(".carrot")) {
+      target.remove();
+      playSound(carrotSound);
+      this.onItemClick && this.onItemClick("carrot");
+    } else if (target.matches(".bug") || target.matches(".bug--crazy")) {
+      this.onItemClick && this.onItemClick("bug");
+    }
+  };
+}
+
+function randomNumber(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function playSound(sound) {
+  sound.currentTime = 0;
+  sound.play();
+}
--- src/main.js
+++ src/main.js
@@ -1,32 +1,51 @@
 "use strict";
 
 import PopUp from "./popup.js";
+import Field from "./field.js";
+
+const playBtn = document.querySelector(".play__btn");
+const playTime = document.querySelector(".play__out");
+const playCount = document.querySelector(".play__count");
+
+const CARROT_NUMBER = 12;
+const BUG_NUMBER = 10;
+const CRAZYBUG_NUMBER = 3;
+
+const GAME_DURATION = 9;
+let started = false;
+let score = 0;
+let countDown = undefined;
+
+const alertSound = new Audio("./sound/alert.wav");
+const bgSound = new Audio("./sound/bg.mp3");
+const bugSound = new Audio("./sound/bug_pull.mp3");
+const winSound = new Audio("./sound/game_win.mp3");
 
 const gameFinishBanner = new PopUp();
 gameFinishBanner.setClickListener(() => {
   startGame();
 });
 
-const playBtn = document.querySelector(".play__btn");
-const playTime = document.querySelector(".play__out");
-const playCount = document.querySelector(".play__count");
-const itemArea = document.querySelector(".item-area");
-const itemBoundary = itemArea.getBoundingClientRect();
+const gameField = new Field(CARROT_NUMBER, BUG_NUMBER, CRAZYBUG_NUMBER);
+gameField.setClickListener(onItemClick);
 
-const CARROT_NUMBER = 12;
-const BUG_NUMBER = 10;
-const CRAZYBUG_NUMBER = 3;
-const CARROT_SIZE = 80;
-const GAME_DURATION = 9;
-let started = false;
-let score = 0;
-let countDown = undefined;
+function onItemClick(item) {
+  if (item === "carrot") {
+    score++;
+    scoreCountDown();
+    if (CARROT_NUMBER === score) {
+      finishGame(true);
+      stopCountDown();
+    }
+  } else if (item === "bug") {
+    finishGame(false);
+    stopCountDown();
+  }
+}
 
-const carrotSound = new Audio("./sound/carrot_pull.mp3");
-const alertSound = new Audio("./sound/alert.wav");
-const bgSound = new Audio("./sound/bg.mp3");
-const bugSound = new Audio("./sound/bug_pull.mp3");
-const winSound = new Audio("./sound/game_win.mp3");
+function scoreCountDown() {
+  playCount.textContent = CARROT_NUMBER - score;
+}
 
 playBtn.addEventListener("click", () => {
   if (started) {
@@ -66,33 +85,12 @@ function finishGame(winOrLose) {
   stopSound(bgSound);
 }
 
-itemArea.addEventListener("click", (event) => {
-  const target = event.target;
-  if (target.matches(".carrot")) {
-    target.remove();
-    score++;
-    scoreCountDown();
-    playSound(carrotSound);
-  }
-  if (CARROT_NUMBER === score) {
-    finishGame(true);
-    stopCountDown();
-  } else if (target.matches(".bug") || target.matches(".bug--crazy")) {
-    finishGame(false);
-    stopCountDown();
-  }
-});
-
 function playSound(sound) {
   sound.currentTime = 0;
   sound.play();
 }
 function stopSound(sound) {
   sound.pause();
-}
-
-function scoreCountDown() {
-  playCount.textContent = CARROT_NUMBER - score;
 }
 
 function showStopBtn() {
@@ -128,32 +126,6 @@ function showTimeAndCount() {
 
 function initGame() {
   score = 0;
-  itemArea.innerHTML = "";
   playCount.innerText = CARROT_NUMBER;
-  deployment("carrot", CARROT_NUMBER, "./img/carrot.png");
-  deployment("bug", BUG_NUMBER, "./img/bug.png");
-  deployment("bug--crazy", CRAZYBUG_NUMBER, "./img/crazybug.png");
-}
-
-function deployment(className, number, imgPath) {
-  const x = 0;
-  const y = 0;
-  const widthX = itemBoundary.width - CARROT_SIZE;
-  const heightY = itemBoundary.height - CARROT_SIZE;
-
-  for (let i = 0; i < number; i++) {
-    const item = document.createElement("img");
-    item.setAttribute("class", className);
-    item.setAttribute("src", imgPath);
-    item.style.position = "absolute";
-    const ranX = randomNumber(x, widthX);
-    const ranY = randomNumber(y, heightY);
-    item.style.left = `${ranX}px`;
-    item.style.top = `${ranY}px`;
-    itemArea.appendChild(item);
-  }
-}
-
-function randomNumber(min, max) {
-  return Math.random() * (max - min) + min;
+  gameField.init();
 }
